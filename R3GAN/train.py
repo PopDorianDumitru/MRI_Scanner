a1123964afbeec2602e94d152eae0af2e19bc119
dorianpop1512@gmail.com <uratenie50@gmail.com>
2025-04-13 21:30:22 +0300

    Scaled down architecture

--- R3GAN/train.py
+++ R3GAN/train.py
@@ -269,8 +269,8 @@ def main(**kwargs):
 
     if opts.preset == 'MRI-128':
         WidthPerStage = [1024, 512, 512, 512, 256, 256]
-        BlocksPerStage = [x for x in [3, 2, 1, 1, 2, 3]]
-        CardinalityPerStage = [x for x in [16, 32, 64, 64, 32, 16]]
+        BlocksPerStage = [x for x in [2, 2, 1, 1, 2, 2]]
+        CardinalityPerStage = [x for x in [16, 16, 32, 32, 16, 8]]
         FP16Stages = [-1, -2]
         NoiseDimension = 64
 
